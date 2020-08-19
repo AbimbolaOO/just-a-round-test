ebc94a5698f665f3dd2ee4d191148afbe8522204
AbimbolaOO <abimbolaolayemiwhyte@gmail.com>
2020-08-19 23:05:58 +0100

    Here we go

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Whyte was here</h1>
       <h2>Honestly i would say its been an amazing jorney</h2>
+      <p>Here we go with this again </p>
     </div>
   );
 }
---
+++ src/Newt.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Newt() {
+  return (
+    <div>
+      <h1>Fuck thid shit hurt alot</h1>
+    </div>
+  );
+}
+
+export default Newt;
